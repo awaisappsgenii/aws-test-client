8a0004c1bd0bd095d25d92a8e08a028642851bfb
Awais Khan <awaisappsgenii@gmail.com>
2023-01-13 17:11:20 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('api').then(data => { setUsers(data.data)} )
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          users.map(user => <p>{user.name}</p>)
+        }
       </header>
     </div>
   );
